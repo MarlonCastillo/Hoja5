2ed042b0501515671a93d7d1f65962645a09e759
lim14373 <lim14373@uvg.edu.gt>
2015-08-26 17:54:23 -0600

    Version 2

--- Hoja5/hoja5.py
+++ Hoja5/hoja5.py
@@ -28,3 +28,40 @@ def proceso(env, Creart, name, RAM, memoria, ins, ins_x_ut):
     
     #Si hay memoria pasa a estado Ready 
     yield RAM.get(memoria) #solicita memoria
+
+#*************************************READY**********************************************
+    #Proceso listo, esperar atencion de CPU
+    completado = 0
+    while completado < ins:
+        #Conectarnos con el CPU
+        with CPU.request() as req:
+            yield req
+            #Determinar instrucciones a realizar
+            if (ins-completado)>=3:
+                eje=3
+            else:
+                eje=(ins-completado)
+            #El tiempo de ejecucion es de 1/(instrucciones por unidad de tiempo) con "eje" instrucciones a realizar 
+            yield env.timeout(eje/ins_x_ut)
+
+#*************************************RUNNING**********************************************
+            #Actualizar el contador de instrucciones a realizar 
+            completado += eje
+ 
+        #Cola: Numero aleatorio si es 1 pasa a  cola waiting y si es 2 pasa a cola ready)
+        Cola = random.randint(1,2)
+
+        #si random es 1 va a cola waiting
+        if Cola == 1 and completado<ins:
+        
+#*************************************Waiting**********************************************   
+            with waiting.request() as req2:
+                yield req2
+                #espera en cola de operaciones I/O
+                yield env.timeout(1)                
+
+    RAM.put(memoria)
+
+    # Se agraga al tiempo Final = (tiempo actual - tiempo  llegada)
+    lista.append (env.now - T_Llegar)
+    TiempoFinal += (env.now - T_Llegar)
